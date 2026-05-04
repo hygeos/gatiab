5d27da1421004fd69991293cf7d59e801defe303
Mustapha Moulana <mm@hygeos.com>
2026-05-04 16:02:49 +0200

    Version 1.1.1

--- gatiab/gatiab.py
+++ gatiab/gatiab.py
@@ -11,7 +11,7 @@ from scipy.integrate import simpson
 from datetime import datetime
 from pathlib import Path
 
-GATIAB_VERSION = '1.1.0'
+GATIAB_VERSION = '1.1.1'
 
 AccelDueToGravity  = 9.80665 # m s-2
 MolarMassAir       = 28.970  # g mol-1 dry air
