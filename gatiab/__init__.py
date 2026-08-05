00659509e2e9bfcd5fe5092f128055fdafea4a69
Mustapha Moulana <mm@hygeos.com>
2026-08-05 11:55:28 +0200

    Re-export constants and version from package init

--- gatiab/__init__.py
+++ gatiab/__init__.py
@@ -1,5 +1,11 @@
 """Gaseous Absorption Transmissions at Instrument Averaged Bands."""
 
+from gatiab.constants import (
+    ACCEL_DUE_TO_GRAVITY,
+    MOLAR_MASS,
+    MOLAR_MASS_AIR,
+    VERSION,
+)
 from gatiab.gatiab import get_binary_mat
 from gatiab.gatiab import vec_float_indexing
 from gatiab.gatiab import get_zatm
@@ -8,10 +14,14 @@ from gatiab.gatiab import ckdmip2od
 from gatiab.gatiab import Gatiab
 
 __all__ = [
+    'ACCEL_DUE_TO_GRAVITY',
+    'MOLAR_MASS',
+    'MOLAR_MASS_AIR',
+    'VERSION',
+    'Gatiab',
     'get_binary_mat',
     'vec_float_indexing',
     'get_zatm',
     'get_bands',
     'ckdmip2od',
-    'Gatiab',
 ]
